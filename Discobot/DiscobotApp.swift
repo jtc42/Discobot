05f10ef0b97a6128b7cd14be2f9c012ad4a12479
Joel T. Collins <me@jtcollins.net>
2023-03-11 17:36:44 +0000

    Initial Commit

---
+++ Discobot/DiscobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscobotApp.swift
+//  Discobot
+//
+//  Created by Joel Collins on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
