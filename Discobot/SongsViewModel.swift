5e4e62e47c3d22089754506150d7aacc1ac28d96
Joel T. Collins <me@jtcollins.net>
2023-03-12 23:20:47 +0000

    More layout improvements

--- Discobot/SongsViewModel.swift
+++ Discobot/SongsViewModel.swift
@@ -86,12 +86,16 @@ struct SongsViewModel: View {
     var body: some View {
         GeometryReader { geometry in
             SnappingScrollView(.vertical, decelerationRate: .fast, showsIndicators: false) {
-                VStack(spacing: 0.0) {
+                VStack(spacing: 0) {
                     ForEach(items) { reccommendation in
                         ForEach(reccommendation.albums) { album in
                             AlbumCardView(album: album)
+                                // Pad and set scroll anchor
+                                // NOTE: I have no idea why this weird layout works, but it does
+                                .padding(.bottom, itemPadding * 0.9)
                                 .scrollSnappingAnchor(.bounds)
-                                .padding(.bottom, itemPadding)
+                                .padding(.bottom, itemPadding * 0.1)
+                                // Set the total frame height to 90% of the viewport (used by AlbumCardView for child sizing)
                                 .frame(maxWidth: .infinity, minHeight: geometry.size.height * 0.9)
                         }
                     }
