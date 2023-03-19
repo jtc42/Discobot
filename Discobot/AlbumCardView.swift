4e51fd4e3d619a7f708412b875800da682874575
Joel T. Collins <me@jtcollins.net>
2023-03-19 10:57:41 +0000

    Refactored and cleaned up

---
+++ Discobot/AlbumCardView.swift
@@ -0,0 +1,276 @@
+//
+//  AlbumCardView.swift
+//  Discobot
+//
+//  Created by Joel Collins on 19/03/2023.
+//
+
+import AVFoundation
+import MusicKit
+import SwiftUI
+import SwiftUIPageView
+
+let cardCornerRadius = 20.0
+
+struct AlbumAddButton: View {
+    let album: Album
+
+    var body: some View {
+        Button(action: {
+            Task {
+                do {
+                    let newAlbum = try await album.with()
+                    print(newAlbum.libraryAddedDate ?? "Not added")
+                    try await MusicLibrary.shared.add(album)
+                } catch {
+                    print("Error reloading album info")
+                }
+            }
+        }) {
+            Image(systemName: "plus")
+                .padding(8)
+                .background(.black.opacity(0.5))
+                .foregroundColor(.white)
+                .clipShape(Circle())
+        }
+    }
+}
+
+struct AlbumCardView: View {
+    // MARK: - Initialisation
+
+    /// Album object for this view instance
+    var album: Album
+    /// Page index of this instance
+    let pageIndex: Int
+
+    /// Recommendation info
+    let recommendationTitle: String?
+    let recommendationReason: String?
+
+    /// Binding to the parent view's currently active playbackId
+    @Binding public var nowPlayingIndex: Int?
+
+    /// Binding to the parent view's currently in-view page index
+    @Binding public var currentIndex: Int
+
+    /// Binding to whether the preview is muted or not
+    @Binding public var previewMuted: Bool
+
+    // MARK: - Colours, UI etc
+
+    var cardColour: Color {
+        return Color(album.artwork?.backgroundColor ?? UIColor.systemGray6.cgColor)
+    }
+
+    var primaryTextColor: Color {
+        return Color(album.artwork?.primaryTextColor ?? UIColor.black.cgColor)
+    }
+
+    var secondaryTextColor: Color {
+        return Color(album.artwork?.tertiaryTextColor ?? UIColor.gray.cgColor)
+    }
+
+    var buttonBackgroundColor: Color {
+        return Color(album.artwork?.secondaryTextColor ?? UIColor.systemGray2.cgColor)
+    }
+
+    var buttonLabelColor: Color {
+        return Color(album.artwork?.backgroundColor ?? UIColor.systemGray6.cgColor)
+    }
+
+    // MARK: - Display state
+
+    var isNearby: Bool {
+        return pageIndex == currentIndex || pageIndex == currentIndex + 1 || pageIndex == currentIndex - 1
+    }
+
+    // MARK: - System music playback
+
+    /// The MusicKit player to use for Apple Music playback.
+    private let player = SystemMusicPlayer.shared
+    /// The state of the MusicKit player to use for Apple Music playback.
+    @ObservedObject private var playerState = SystemMusicPlayer.shared.state
+
+    /// `true` when the player is playing.
+    private var isPlaying: Bool {
+        return (playerState.playbackStatus == .playing)
+    }
+
+    /// `true` when this album is currently active (may be paused).
+    private var nowPlayingThisAlbum: Bool {
+        return (nowPlayingIndex == pageIndex)
+    }
+
+    /// `true` when this album is currently active  and playing
+    private var isPlayingThisAlbum: Bool {
+        return isPlaying && nowPlayingThisAlbum
+    }
+
+    /// A declaration of the Play/Pause button, and (if appropriate) the Join button, side by side.
+    private var primaryButtons: some View {
+        HStack {
+            // Open in Apple Music button
+            Button(action: {
+                if let url = album.url {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }) {
+                HStack {
+                    Image(systemName: "arrow.up.forward.app")
+                    Text("Open")
+                }
+                .frame(maxWidth: .infinity, maxHeight: 18)
+            }
+            .padding(15)
+            .background(self.buttonBackgroundColor.cornerRadius(8))
+
+            // Play button
+            Button(action: handlePlayButtonSelected) {
+                HStack {
+                    Image(systemName: isPlayingThisAlbum ? "pause.fill" : "play.fill")
+                    Text(isPlayingThisAlbum ? "Pause" : "Play")
+                }
+                .frame(maxWidth: .infinity, maxHeight: 18)
+            }
+            .padding(15)
+            .background(self.buttonBackgroundColor.cornerRadius(8))
+            .animation(.easeInOut(duration: 0.1), value: isPlayingThisAlbum)
+
+        }.font(.body.bold()).foregroundColor(self.buttonLabelColor)
+    }
+
+    /// The action to perform when the user taps the Play/Pause button.
+    private func handlePlayButtonSelected() {
+        // If this album isn't actively playing
+        if !isPlayingThisAlbum {
+            // If this album isn't active
+            if !nowPlayingThisAlbum {
+                // Set this album as the queue and start playing
+                player.queue = [album]
+                beginPlaying()
+            } else { // If this album is active, but not playing right now
+                // Resume
+                Task {
+                    do {
+                        try await player.play()
+                    } catch {
+                        print("Failed to resume playing with error: \(error).")
+                    }
+                }
+            }
+        } else {
+            player.pause()
+        }
+    }
+
+    /// A convenience method for beginning music playback and setting the nowPlayingId.
+    ///
+    /// Call this instead of `MusicPlayer`’s `play()`
+    /// method whenever the playback queue is reset.
+    private func beginPlaying() {
+        Task {
+            do {
+                // Try playing with the system music player
+                try await player.play()
+                // Mute previews now something is actually playing
+                previewMuted = true
+                // Update the shared state for which item is playing
+                nowPlayingIndex = pageIndex
+            } catch {
+                print("Failed to prepare to play with error: \(error).")
+            }
+        }
+    }
+
+    // MARK: - View body
+
+    var body: some View {
+        GeometryReader { geometry in
+            // Height of VStack is determined by it's children, and the height of the children
+            // are in turn determined by the available height set by the frame of this view (measured by GeometryReader)
+            LazyVStack(alignment: .leading) {
+                // Get art size from the parent geometry multiplied by display scale
+                let artSize = geometry.size.width * UIScreen().scale
+                AsyncImage(
+                    url: album.artwork?.url(width: Int(artSize), height: Int(artSize)),
+                    content: { image in
+                        image.resizable().aspectRatio(contentMode: .fit)
+                    },
+                    placeholder: {
+                        Image(systemName: "a.square") // TODO: Replace with a better placeholder
+                            // Make placeholder resizable
+                            .resizable()
+                            // Force to a square, so cards with placeholders are roughly the right size
+                            .aspectRatio(1, contentMode: .fit)
+                    }
+                )
+                .frame(width: geometry.size.width)
+                .overlay(alignment: .topTrailing) {
+                    Button(action: {
+                        previewMuted = !previewMuted
+                    }) {
+                        Image(systemName: previewMuted ? "speaker.slash.fill" : "speaker.fill")
+                            .padding(8)
+                            .background(.black.opacity(0.5))
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                    }.padding(12)
+                }
+
+                // Everything other than album art
+                VStack(alignment: .leading, spacing: 16.0) {
+                    // AlbumAddButton(album: album)
+
+                    // OPTIONAL: Initial spacer to push item info down
+                    Spacer()
+
+                    // Recommendation info
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        if let recommendationTitle = recommendationTitle {
+                            Text(recommendationTitle)
+                                .font(Font.system(.caption))
+                        }
+                        if let recommendationReason = recommendationReason {
+                            Text(recommendationReason)
+                                .font(.system(.caption2))
+                        }
+                    }.foregroundColor(self.secondaryTextColor)
+
+                    // Item info
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text(album.title)
+                            .font(Font.system(.headline))
+                        Text(album.artistName)
+                            .font(.system(.subheadline))
+                    }
+                    .foregroundColor(self.primaryTextColor)
+                    .padding(.top, 8.0)
+
+                    // Push all remaining content to the bottom of the available space
+                    Spacer()
+
+                    // Item card - Full width and horizontally centered
+                    VStack(spacing: 16.0) {
+                        // Buttons seem to make the PageView scroll lag like heck,
+                        // so only render them if the page is in view
+                        if isNearby {
+                            primaryButtons
+                        }
+                    }.frame(maxWidth: .infinity, alignment: .center)
+                }
+                // Padding, for a e s t h e t i c
+                .padding([.horizontal, .bottom], 20.0)
+                // Lock height as the difference between the artwork height and the full available height
+                .frame(width: geometry.size.width, height: geometry.size.height - geometry.size.width, alignment: .topLeading)
+                // Apply a gradient background to the bottom half of the card
+                .background(Color(album.artwork?.backgroundColor ?? UIColor.systemGray6.cgColor))
+            }
+            .cornerRadius(cardCornerRadius)
+            .overlay( /// Rounded border
+                RoundedRectangle(cornerRadius: cardCornerRadius).stroke(Color(UIColor.systemGray4), lineWidth: 0.5)
+            )
+            .shadow(radius: 16.0)
+        }
+    }
+}
--- Discobot/SongsViewModel.swift
+++ Discobot/SongsViewModel.swift
@@ -15,268 +15,6 @@ import MusicKit
 import SwiftUI
 import SwiftUIPageView
 
-let cardCornerRadius = 20.0
-
-struct AlbumCardView: View {
-    // MARK: - Initialisation
-
-    /// Album object for this view instance
-    var album: Album
-    /// Page index of this instance
-    let pageIndex: Int
-
-    /// Recommendation info
-    let recommendationTitle: String?
-    let recommendationReason: String?
-
-    /// Binding to the parent view's currently active playbackId
-    @Binding public var nowPlayingIndex: Int?
-
-    /// Binding to the parent view's currently in-view page index
-    @Binding public var currentIndex: Int
-
-    /// Binding to whether the preview is muted or not
-    @Binding public var previewMuted: Bool
-
-    // MARK: - Colours, UI etc
-
-    var cardColour: Color {
-        return Color(album.artwork?.backgroundColor ?? UIColor.systemGray6.cgColor)
-    }
-
-    var primaryTextColor: Color {
-        return Color(album.artwork?.primaryTextColor ?? UIColor.black.cgColor)
-    }
-
-    var secondaryTextColor: Color {
-        return Color(album.artwork?.tertiaryTextColor ?? UIColor.gray.cgColor)
-    }
-
-    var buttonBackgroundColor: Color {
-        return Color(album.artwork?.secondaryTextColor ?? UIColor.systemGray2.cgColor)
-    }
-
-    var buttonLabelColor: Color {
-        return Color(album.artwork?.backgroundColor ?? UIColor.systemGray6.cgColor)
-    }
-
-    // MARK: - Display state
-
-    var isNearby: Bool {
-        return pageIndex == currentIndex || pageIndex == currentIndex + 1 || pageIndex == currentIndex - 1
-    }
-
-    // MARK: - System music playback
-
-    /// The MusicKit player to use for Apple Music playback.
-    private let player = SystemMusicPlayer.shared
-    /// The state of the MusicKit player to use for Apple Music playback.
-    @ObservedObject private var playerState = SystemMusicPlayer.shared.state
-
-    /// `true` when the player is playing.
-    private var isPlaying: Bool {
-        return (playerState.playbackStatus == .playing)
-    }
-
-    /// `true` when this album is currently active (may be paused).
-    private var nowPlayingThisAlbum: Bool {
-        return (nowPlayingIndex == pageIndex)
-    }
-
-    /// `true` when this album is currently active  and playing
-    private var isPlayingThisAlbum: Bool {
-        return isPlaying && nowPlayingThisAlbum
-    }
-
-    /// `true` when the album detail view needs to disable the Play/Pause button.
-//    private var isPlayButtonDisabled: Bool {
-//        let canPlayCatalogContent = musicSubscription?.canPlayCatalogContent ?? false
-//        return !canPlayCatalogContent
-//    }
-    var isPlayButtonDisabled: Bool = false
-
-    /// A declaration of the Play/Pause button, and (if appropriate) the Join button, side by side.
-    private var primaryButtons: some View {
-        HStack {
-            // Open in Apple Music button
-            Button(action: {
-                if let url = album.url {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            }) {
-                HStack {
-                    Image(systemName: "arrow.up.forward.app")
-                    Text("Open")
-                }
-                .frame(maxWidth: .infinity, maxHeight: 18)
-            }
-            .padding(15)
-            .background(self.buttonBackgroundColor.cornerRadius(8))
-
-            // Play button
-            Button(action: handlePlayButtonSelected) {
-                HStack {
-                    Image(systemName: isPlayingThisAlbum ? "pause.fill" : "play.fill")
-                    Text(isPlayingThisAlbum ? "Pause" : "Play")
-                }
-                .frame(maxWidth: .infinity, maxHeight: 18)
-            }
-            .padding(15)
-            .background(self.buttonBackgroundColor.cornerRadius(8))
-            .disabled(isPlayButtonDisabled)
-            .animation(.easeInOut(duration: 0.1), value: isPlayingThisAlbum)
-//            if shouldOfferSubscription {
-//                subscriptionOfferButton
-//            }
-
-            // Add to library button (I really want an Open in Apple Music button instead...)
-//            Button(action: { }) {
-//                HStack {
-//                    Image(systemName: (album.libraryAddedDate != nil) ? "checkmark" : "plus")
-//                    Text((album.libraryAddedDate != nil) ? "Added" : "Add to Library")
-//                }
-//                .frame(maxWidth: .infinity, maxHeight: 18)
-//            }.font(.body.bold())
-//                .foregroundColor(self.buttonLabelColor)
-//                .padding(15)
-//                .background(self.buttonBackgroundColor.cornerRadius(8))
-//                .animation(.easeInOut(duration: 0.1), value: album.libraryAddedDate != nil)
-        }.font(.body.bold()).foregroundColor(self.buttonLabelColor)
-    }
-
-    /// The action to perform when the user taps the Play/Pause button.
-    private func handlePlayButtonSelected() {
-        // If this album isn't actively playing
-        if !isPlayingThisAlbum {
-            // If this album isn't active
-            if !nowPlayingThisAlbum {
-                // Set this album as the queue and start playing
-                player.queue = [album]
-                beginPlaying()
-            } else { // If this album is active, but not playing right now
-                // Resume
-                Task {
-                    do {
-                        try await player.play()
-                    } catch {
-                        print("Failed to resume playing with error: \(error).")
-                    }
-                }
-            }
-        } else {
-            player.pause()
-        }
-    }
-
-    /// A convenience method for beginning music playback and setting the nowPlayingId.
-    ///
-    /// Call this instead of `MusicPlayer`’s `play()`
-    /// method whenever the playback queue is reset.
-    private func beginPlaying() {
-        Task {
-            do {
-                // Try playing with the system music player
-                try await player.play()
-                // Mute previews now something is actually playing
-                previewMuted = true
-                // Update the shared state for which item is playing
-                nowPlayingIndex = pageIndex
-            } catch {
-                print("Failed to prepare to play with error: \(error).")
-            }
-        }
-    }
-
-    // MARK: - View body
-
-    var body: some View {
-        GeometryReader { geometry in
-            // Height of VStack is determined by it's children, and the height of the children
-            // are in turn determined by the available height set by the frame of this view (measured by GeometryReader)
-            LazyVStack(alignment: .leading) {
-                // Get art size from the parent geometry multiplied by display scale
-                let artSize = geometry.size.width * UIScreen().scale
-                AsyncImage(
-                    url: album.artwork?.url(width: Int(artSize), height: Int(artSize)),
-                    content: { image in
-                        image.resizable().aspectRatio(contentMode: .fit)
-                    },
-                    placeholder: {
-                        Image(systemName: "a.square") // TODO: Replace with a better placeholder
-                            // Make placeholder resizable
-                            .resizable()
-                            // Force to a square, so cards with placeholders are roughly the right size
-                            .aspectRatio(1, contentMode: .fit)
-                    }
-                )
-                .frame(width: geometry.size.width)
-                .overlay(alignment: .topTrailing) {
-                    Button(action: {
-                        previewMuted = !previewMuted
-                    }) {
-                        Image(systemName: previewMuted ? "speaker.slash.fill" : "speaker.fill")
-                            .padding(8)
-                            .background(.black.opacity(0.5))
-                            .foregroundColor(.white)
-                            .clipShape(Circle())
-                    }.padding(12)
-                }
-
-                // Everything other than album art
-                VStack(alignment: .leading, spacing: 16.0) {
-                    // OPTIONAL: Initial spacer to push item info down
-                    Spacer()
-
-                    // Recommendation info
-                    VStack(alignment: .leading, spacing: 8.0) {
-                        if let recommendationTitle = recommendationTitle {
-                            Text(recommendationTitle)
-                                .font(Font.system(.caption))
-                        }
-                        if let recommendationReason = recommendationReason {
-                            Text(recommendationReason)
-                                .font(.system(.caption2))
-                        }
-                    }.foregroundColor(self.secondaryTextColor)
-
-                    // Item info
-                    VStack(alignment: .leading, spacing: 8.0) {
-                        Text(album.title)
-                            .font(Font.system(.headline))
-                        Text(album.artistName)
-                            .font(.system(.subheadline))
-                    }
-                    .foregroundColor(self.primaryTextColor)
-                    .padding(.top, 8.0)
-
-                    // Push all remaining content to the bottom of the available space
-                    Spacer()
-
-                    // Item card - Full width and horizontally centered
-                    VStack(spacing: 16.0) {
-                        // Buttons seem to make the PageView scroll lag like heck,
-                        // so only render them if the page is in view
-                        if isNearby {
-                            primaryButtons
-                        }
-                    }.frame(maxWidth: .infinity, alignment: .center)
-                }
-                // Padding, for a e s t h e t i c
-                .padding([.horizontal, .bottom], 20.0)
-                // Lock height as the difference between the artwork height and the full available height
-                .frame(width: geometry.size.width, height: geometry.size.height - geometry.size.width, alignment: .topLeading)
-                // Apply a gradient background to the bottom half of the card
-                .background(Color(album.artwork?.backgroundColor ?? UIColor.systemGray6.cgColor))
-            }
-            .cornerRadius(cardCornerRadius)
-            .overlay( /// Rounded border
-                RoundedRectangle(cornerRadius: cardCornerRadius).stroke(Color(UIColor.systemGray4), lineWidth: 0.5)
-            )
-            .shadow(radius: 16.0)
-        }
-    }
-}
-
 struct FeedItem: Identifiable, Hashable {
     var id = UUID()
     let recommendationReason: String?
@@ -285,9 +23,12 @@ struct FeedItem: Identifiable, Hashable {
 }
 
 struct SongsViewModel: View {
+    /// Flattened array of feed items
     @State var flatItems: [FeedItem] = []
 
+    /// Page index of the item currently playing in the system player
     @State var nowPlayingIndex: Int? = nil
+    /// Page index of the item currently in view
     @State var currentIndex: Int = 0
 
     @State var previewMuted: Bool = true
@@ -296,7 +37,11 @@ struct SongsViewModel: View {
     // Track if the user has changed page yet (used to auto-unmute)
     @State var firstPageMove: Bool = true
 
+    // Padding between pages
     let itemPadding = 40.0
+
+    /// The color scheme of the environment.
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
         GeometryReader { geometry in
@@ -407,8 +152,6 @@ struct SongsViewModel: View {
             } catch {
                 print(String(describing: error))
             }
-
-        // Assign songs
         default:
             break
         }
